48ecb582553e0221d37755f8873aff06449c36be
richardsrobinr <richardsrobin.r15@gmail.com>
2025-02-22 19:47:55 +0530

    renamed mainpage to widgetspage in url

--- Aim/todo/urls.py
+++ Aim/todo/urls.py
@@ -20,6 +20,7 @@ from todo import  views
 
 urlpatterns = [
     path('',views.main_page,name='main_page'),
+    path('',views.main_page,name='widgets_page'),
     path('<int:id>/update', views.update, name='update'),
     path('<int:id>/delete', views.delete, name='delete'),
     path('add/', views.add, name='add'),
